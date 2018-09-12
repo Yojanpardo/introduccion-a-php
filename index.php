cbe2a7ec90874f9256b08bdfeb952d893a6e1b29
Yojan Antonio Pardo Zabala <yojan.pardo@gmail.com>
2018-09-11 22:07:49 -0500

    empiezo los ejercicios

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
     <title></title>
   </head>
   <body>
-    <?php echo 'hola perritos' ?>
+    <h1>Programación con php</h1>
+    <h3>ejercicio para imprimir lo solicitado por el profe con el arreglo dado</h3>
+    <?php
+      $arreglo = [
+	    ‘keyStr1’ => ‘lado’,
+      0 => ‘ledo’,
+	    ‘keyStr2’ => ‘lido’,
+	    1 => ‘lodo’,
+	    2 => ‘ludo’
+    ];
+      echo "<p>$arreglo[‘keyStr1’], $arreglo[0], $arreglo[‘keyStr2’], $arreglo[1], $arreglo[2]</p>";
+      echo "<p>Decirlo al revéz lo dudo.</p>";
+      echo "<p>$arreglo[2], $arreglo[1], $arreglo[‘keyStr2’], $arreglo[0], $arreglo[‘keyStr1’]</p>";
+      echo "<p>Qué trabajo me ha costado</p>";
+    ?>
   </body>
 </html>
